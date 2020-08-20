8f05c689fada95f57b4be5b8d021cc1131c80c1d
WCFX <wcfx.dev@gmail.com>
2020-08-20 13:39:42 -0300

    Criando o corpo do projeto 2.1

--- src/components/ProfileData/styles.ts
+++ src/components/ProfileData/styles.ts
@@ -2,11 +2,28 @@ import styled, { css } from 'styled-components';
 
 import { RiGroupLine, RiBuilding4Line, RiMapPin2Line, RiMailLine, RiLinksLine } from 'react-icons/ri';
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+
+  > div {
+    margin-left: 24px;
+
+    > h1 {
+      font-size: 26px;
+      line-height: 1.25;
+      color: var(--gray-dark);
+      font-weight: 600;
+    }
+    > h2 {
+      font-size: 20px;
+      color: var(--username);
+    }
+  }
 
 `;
-
-export const Flex = styled.div``;
 
 export const Avatar = styled.img``;
 
